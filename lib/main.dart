00dc0b6b059e27f3711f87cd8f810915c0c5d964
Mukesh Soni <msony552@gmail.com>
2020-12-30 18:30:44 +0530

    Laps counter widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,21 @@
 import 'package:flutter/material.dart';
 import 'dart:async';
 
+import 'package:timer/widgets/laps_widget.dart';
+
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
         visualDensity: VisualDensity.adaptivePlatformDensity,
+        primaryColor: Colors.redAccent,
+        accentColor: Colors.yellowAccent,
       ),
       home: MyHomePage(title: 'Timer'),
     );
@@ -33,18 +36,19 @@ class _MyHomePageState extends State<MyHomePage> {
   void _startTimer() {
     const oneSec = Duration(seconds: 1);
     _timer = new Timer.periodic(oneSec, (Timer timer) {
-        setState(() {
-          if(_counter <= 0) {
-            _timer.cancel();
-          }else   _counter--;
-        });
-
+      setState(() {
+        if (_counter <= 0) {
+          _timer.cancel();
+        } else
+          _counter--;
+      });
     });
   }
 
-  void _stopTimer(){
+  void _stopTimer() {
     _timer.cancel();
   }
+
   @override
   void dispose() {
     super.dispose();
@@ -61,27 +65,49 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
-              '$_counter',
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 40,
-              ),
+            Expanded(
+              child: Container(
+                  color: Colors.blue,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        '$_counter',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 40,
+                        ),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          FlatButton(
+                            child: const Text("Start timer"),
+                            onPressed: () {
+                              if (_timer != null) _timer.cancel();
+                              _counter = 10;
+                              _startTimer();
+                            },
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(100.0),
+                            ),
+
+                          ),
+                          FlatButton(
+                            color: Theme.of(context).primaryColor,
+
+                            child: const Text("Stop timer"),
+                            onPressed: () => _stopTimer(),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(100.0),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  )),
             ),
-            RaisedButton(
-              onPressed: () {
-                if(_timer != null) _timer.cancel();
-                _counter = 10;
-                _startTimer();
-                },
-              elevation: 100,
-              child: const Text("Start timer"),
-            ),
-            RaisedButton(
-              onPressed: () => _stopTimer(),
-              elevation: 100,
-              child: Text("Stop timer"),
-            )
+            ListLaps()
           ],
         ),
       ),
---
+++ lib/widgets/laps_widget.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ListLaps extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 500,
+      width: 300,
+      color: Colors.red,
+      child: Column(
+        children: [
+
+        ],
+      ),
+    );
+  }
+}
